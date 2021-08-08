4dbc3973226b6a8b1714fbaacef49fd606d77259
Joel T George <77021448+Joel-T-George@users.noreply.github.com>
2021-08-08 22:44:23 +0530

    Add files via upload

--- Automatic-ICMR_portal-v0.1/popup.js
+++ Automatic-ICMR_portal-v0.1/popup.js
@@ -1,34 +1,4 @@
-var selectedExcelFile = "sx.xlsx"
-var rowobject ;
-document.getElementById("Excelfileselection").addEventListener("change",(eventhappens)=>{
-	selectedExcelFile = eventhappens.target.files[0]
-	
-})
-// Getting Data From Excel File and Convert to Json Format 
-document.getElementById("filesubmit").addEventListener("click",()=>{
-	if(selectedExcelFile){
-		let fileRead = new FileReader()
-		fileRead.readAsBinaryString(selectedExcelFile)
-		fileRead.onload = (eventhappens)=>{
-			// binary format data produces in code
-			var data =eventhappens.target.result
-			var workbook  = XLSX.read(data,{type:"binary"})
-			// convert row to jsonformat Objects with sheets
-			workbook.SheetNames.forEach(sheet =>{
-				var rowobject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
-				console.log(rowobject)
-				
-				document.getElementById("GetInputFile").style.display = "none" 
-				document.getElementById("Showprofiles").style.display = "block"
-				ShowProfiles(rowobject)
-			})
-			
-		}
-		
-		
-		
-	}
-})
+
 function ShowProfiles(data){
 	console.log(data[0][" Patient Name"])
 	var Profile = document.getElementById("count")
@@ -53,4 +23,39 @@ function ShowProfiles(data){
 		Profile.innerHTML = "data not present in excelfile"
 	}
 }
+
+function SendDataToPage(data){
+
+}
+
+var selectedExcelFile = "sx.xlsx"
+var rowobject = {}
+
+document.getElementById("Excelfileselection").addEventListener("change",(eventhappens)=>{
+	selectedExcelFile = eventhappens.target.files[0]
+	
+})
+
+// Getting Data From Excel File and Convert to Json Format 
+document.getElementById("filesubmit").addEventListener("click",()=>{
+	if(selectedExcelFile){
+		let fileRead = new FileReader()
+		fileRead.readAsBinaryString(selectedExcelFile)
+		fileRead.onload = (eventhappens)=>{
+			// binary format data produces in code
+			var data =eventhappens.target.result
+			var workbook  = XLSX.read(data,{type:"binary"})
+			// convert row to jsonformat Objects with sheets
+			workbook.SheetNames.forEach(sheet =>{
+				var rowobject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
+				console.log(rowobject)
+				
+				document.getElementById("GetInputFile").style.display = "none" 
+				document.getElementById("Showprofiles").style.display = "block"
+
+				ShowProfiles(rowobject)
+			})
+		}
+	}
+})
 
